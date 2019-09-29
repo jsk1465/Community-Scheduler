00f5cdd7f827551f846034ff23ea1277e80dfc95
Jonathon Kirkpatrick <jon.kirkpatrick@sbcglobal.net>
2019-09-29 08:05:10 -0500

    test

--- scheduler_bot/index.js
+++ scheduler_bot/index.js
@@ -11,23 +11,198 @@ let bot = new SlackBot({
 });
 
 let atchData = JSON.parse(fs.readFileSync('../InputForms.json'));
-let formData = atchData;
+// let formData = [{
+// 	"blocks": [
+// 		{
+// 			"type": "actions",
+// 			"elements": [
+// 				{
+// 					"type": "static_select",
+// 					"placeholder": {
+// 						"type": "plain_text",
+// 						"text": "Select an item",
+// 						"emoji": true
+// 					},
+// 					"options": [
+// 						{
+// 							"text": {
+// 								"type": "plain_text",
+// 								"text": "Excellent item 1",
+// 								"emoji": true
+// 							},
+// 							"value": "value-0"
+// 						},
+// 						{
+// 							"text": {
+// 								"type": "plain_text",
+// 								"text": "Fantastic item 2",
+// 								"emoji": true
+// 							},
+// 							"value": "value-1"
+// 						},
+// 						{
+// 							"text": {
+// 								"type": "plain_text",
+// 								"text": "Nifty item 3",
+// 								"emoji": true
+// 							},
+// 							"value": "value-2"
+// 						},
+// 						{
+// 							"text": {
+// 								"type": "plain_text",
+// 								"text": "Pretty good item 4",
+// 								"emoji": true
+// 							},
+// 							"value": "value-3"
+// 						}
+// 					]
+// 				},
+// 				{
+// 					"type": "static_select",
+// 					"placeholder": {
+// 						"type": "plain_text",
+// 						"text": "Select an item",
+// 						"emoji": true
+// 					},
+// 					"options": [
+// 						{
+// 							"text": {
+// 								"type": "plain_text",
+// 								"text": "Excellent item 1",
+// 								"emoji": true
+// 							},
+// 							"value": "value-0"
+// 						},
+// 						{
+// 							"text": {
+// 								"type": "plain_text",
+// 								"text": "Fantastic item 2",
+// 								"emoji": true
+// 							},
+// 							"value": "value-1"
+// 						},
+// 						{
+// 							"text": {
+// 								"type": "plain_text",
+// 								"text": "Nifty item 3",
+// 								"emoji": true
+// 							},
+// 							"value": "value-2"
+// 						},
+// 						{
+// 							"text": {
+// 								"type": "plain_text",
+// 								"text": "Pretty good item 4",
+// 								"emoji": true
+// 							},
+// 							"value": "value-3"
+// 						}
+// 					]
+// 				},
+// 				{
+// 					"type": "static_select",
+// 					"placeholder": {
+// 						"type": "plain_text",
+// 						"text": "Select an item",
+// 						"emoji": true
+// 					},
+// 					"options": [
+// 						{
+// 							"text": {
+// 								"type": "plain_text",
+// 								"text": "Excellent item 1",
+// 								"emoji": true
+// 							},
+// 							"value": "value-0"
+// 						},
+// 						{
+// 							"text": {
+// 								"type": "plain_text",
+// 								"text": "Fantastic item 2",
+// 								"emoji": true
+// 							},
+// 							"value": "value-1"
+// 						},
+// 						{
+// 							"text": {
+// 								"type": "plain_text",
+// 								"text": "Nifty item 3",
+// 								"emoji": true
+// 							},
+// 							"value": "value-2"
+// 						},
+// 						{
+// 							"text": {
+// 								"type": "plain_text",
+// 								"text": "Pretty good item 4",
+// 								"emoji": true
+// 							},
+// 							"value": "value-3"
+// 						}
+// 					]
+// 				},
+// 				{
+// 					"type": "static_select",
+// 					"placeholder": {
+// 						"type": "plain_text",
+// 						"text": "Select an item",
+// 						"emoji": true
+// 					},
+// 					"options": [
+// 						{
+// 							"text": {
+// 								"type": "plain_text",
+// 								"text": "Excellent item 1",
+// 								"emoji": true
+// 							},
+// 							"value": "value-0"
+// 						},
+// 						{
+// 							"text": {
+// 								"type": "plain_text",
+// 								"text": "Fantastic item 2",
+// 								"emoji": true
+// 							},
+// 							"value": "value-1"
+// 						},
+// 						{
+// 							"text": {
+// 								"type": "plain_text",
+// 								"text": "Nifty item 3",
+// 								"emoji": true
+// 							},
+// 							"value": "value-2"
+// 						},
+// 						{
+// 							"text": {
+// 								"type": "plain_text",
+// 								"text": "Pretty good item 4",
+// 								"emoji": true
+// 							},
+// 							"value": "value-3"
+// 						}
+// 					]
+// 				}
+// 			]
+// 		}
+// 	]
+// }]
+
+formData = {attachments:formData}
+
 console.log(formData.attachments);
 
 bot.on('start', () => {
     // more information about additional params https://api.slack.com/methods/chat.postMessage
     let params = {};
     console.log('started');
-    
+    // console.log(bot.getUsers()._value.members)
     // define channel, where bot exist. You can adjust it there https://my.slack.com/services 
-    bot.postMessageToChannel(
-        'testform',
-        'Hi!',
-        params);
-});
-
-bot.on('error', () => {
-    console.log(err);
+    // bot.postMessageToChannel(
+    //     'testform',
+    //     'Hi!',
+    //     params);
 });
 
 bot.on('message', data => {
@@ -38,7 +213,7 @@ bot.on('message', data => {
     let usr = data.user;
     if(msg==='\\h'){
         console.log('found someone to help!')
-        msg = "test"
+        msg = "Hello person :)"
         bot.postMessageToChannel(
             'testform',
             msg,
@@ -47,4 +222,9 @@ bot.on('message', data => {
     else{
         console.log(data);
     }
+});
+
+bot.on('open', data => {
+    // console.log(data);
+    console.log('open biiiiii');
 });
